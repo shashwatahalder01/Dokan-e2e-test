d683441fc74c4636d96a0372761a0fcbe4b4ac67
shashwata <shashwatahalder01@gmail.com>
2022-05-26 13:17:57 +0600

    abc

--- tests/e2e/tests/admin.spec.js
+++ tests/e2e/tests/admin.spec.js
@@ -22,11 +22,11 @@ describe('admin functionality test', () => {
         await adminPage.setDokanSettings()
     }, timeout)
 
-    it('admin can login', async () => {
+    it.only('admin can login', async () => {
         await loginPage.adminLogin(process.env.ADMIN, process.env.ADMIN_PASSWORD)
     }, timeout)
 
-    it('admin can logout', async () => {
+    it.only('admin can logout', async () => {
         await loginPage.adminLogin(process.env.ADMIN, process.env.ADMIN_PASSWORD)
         await adminPage.adminLogout()
     }, timeout)
